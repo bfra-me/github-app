d6cf115bfd612febad303d9ab92127ac599661ff
Marcus R. Brown <git@mrbro.dev>
2024-11-24 23:42:30 -0700

    feat: overhaul (#167)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /**
  * @param {import('probot').Probot} app
  */
-module.exports = (app) => {
-  app.on('issues.opened', async (context) => {
-    return context.octokit.issues.createComment(context.issue({body: 'Hello, World!'}));
-  });
-};
+export default app => {
+  app.on('issues.opened', async context => {
+    return context.octokit.issues.createComment(context.issue({body: 'Hello, World!'}))
+  })
+}
