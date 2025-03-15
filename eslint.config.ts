a1ab35f8574f2f8cf566676623ca972ac0d5828c
Marcus R. Brown <git@mrbro.dev>
2025-03-15 04:47:49 -0700

    chore: update ESLint config, VS Code settings, and repo settings (#296)

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,1 +1,11 @@
-export {default} from '@bfra.me/eslint-config'
+import {defineConfig, type Config} from '@bfra.me/eslint-config'
+
+const config: ReturnType<typeof defineConfig> = defineConfig({
+  name: '@bfra.me/.github',
+  ignores: ['**/dist/**', '**/node_modules/**'],
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
+})
+
+export default config as Config
